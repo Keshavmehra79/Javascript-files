3149c68209505bdccfc25a21ebe68823623eef1a
keshavmehra79 <mehrakeshav332@gmail.com>
2025-06-20 13:11:46 +0530

    first js

---
+++ cn.js
@@ -0,0 +1,54 @@
+// let num=parseInt(prompt("Enter Number: "))
+// if(num>0){
+//     console.log("Number is positive")
+
+// }
+// else{
+//     console.log("Number is negative")
+// }
+
+//wap to take inputp from the user and check number is even or odd
+// let num=parseInt(prompt("Enter Number: "))
+// if(num%2==0){
+//     console.log("Number is even")
+
+// }
+// else{
+//     console.log("Number is odd")
+// }
+// wap to cjeck num is posititve or neg or nutrral
+// let num=parseInt(prompt("Enter Number: "))
+// if(num>0){
+//     console.log("Number is pos")
+
+// }
+// else if(num<0){
+//     console.log("Number is neg")
+// }
+// else{
+//     console.log("Number is nutral")
+// }
+//wap to chek num is odd or even if num is odd then print area of circle 
+// of thet number if num is pos then print cube of that number 
+// let num=parseInt(prompt("Enter Number: "))
+// if(num%2!=0){
+//     console.log("area of circle: " ,3.14*num*num)
+
+// }
+// else{
+// console.log("cube: ",num*num*num)
+
+// }
+// let age=parseInt(prompt("Enter your age"))
+// if(age>=18){
+//    let  num=parseInt(prompt("Press 1 for Indian "))
+//     if(num==1){
+//         console.log("you Are eligible")
+//     }
+//     else{
+//         console.log("you are not indian")
+//     }
+// }
+// else{
+//     console.log("your are underage")
+// }
---
+++ loops.js
@@ -0,0 +1,84 @@
+// var n=1
+// do{
+//     console.log(n);
+//     n++;
+// }while(n<11);
+
+//wap to print are table of n numbers
+
+// var n=parseInt(prompt("Enter a number: "))
+// var i=1
+// do{
+//     console.log(n*i);
+//     i++;
+    
+// }while(i<11);
+
+//wap to print micrpssoft 15 times
+// var n=1
+// while(n<16){
+//     // console.log("Microsoft")
+//     document.write("Microsoft","<br>")
+//     n++;
+// }
+//wap to reverse table of n number using while loop
+// var n=parseInt(prompt("enter a number: "))
+// var i=10
+// while(i>=1){
+//     console.log(n*i)
+
+//     i--
+// }
+//wap to print only odd numbers b/w 20 to 1
+// n=20
+// while(n>=1){
+//     if(n%2!=0){
+//         console.log(n)
+//     }
+//     n--
+
+// }
+///////////////FOR////////////
+// for(let n=1;n<=10;n++){
+//     console.log(n);
+// }
+
+// WAP
+// var a =parseInt(prompt("Enter a number"))
+// if(a>=1 && a<=10){
+//     for(let n=1;n<=a;n++){
+//         console.log(n)
+//     }
+// }
+// else if(a>=11 && a<=20){
+//     for(let n=20;n>=1;n--){
+//         console.log(n)
+//     }
+// }
+// else{
+//     console.log("Invalid")
+// }
+//wap to print square of all even numbers between 20 to 1
+// for(let n=20;n>=1;n--){
+//     if(n%2==0){
+//         console.log(n*n)
+//         n--
+//     }
+// }
+
+// for(let a=2; a<=3;a++){
+//     for(let b=1;b<=10;b++){
+//         console.log(a*b)
+//     }
+// }
+
+for(let a=1;a<=5;a++){
+    for(let a=1;a<=5;a++){
+        if(a>=1){
+        document.write("*"," ")
+    }
+    else{
+        document.write(" ")
+    }
+}
+}
