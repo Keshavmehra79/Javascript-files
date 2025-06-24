6aa737586c2ee50743c8e3c3c80475162d7aa504
keshavmehra79 <mehrakeshav332@gmail.com>
2025-06-24 09:50:54 +0530

    24june

--- array.js
+++ array.js
@@ -5,7 +5,7 @@
 //     console.log(ar[i])
 // }
 
-let a=[1,2,3,4,5,6,7,8,9,10]
+// let a=[1,2,3,4,5,6,7,8,9,10]
 // for(let i=0;i<10;i++){
 //     console.log(a[i])
 
@@ -18,7 +18,39 @@ let a=[1,2,3,4,5,6,7,8,9,10]
     
 // }
 
-for(let vl of a){
-    console.log(vl)
-}
+// for(let vl of a){
+//     console.log(vl)
+// }
 
+//push
+// let ar=['qSIM',45,'BHOPL',5.67]
+// ar.push("India","MP")
+// console.log(ar)
+// //pop
+// ar.pop()
+// console.log(ar);
+// //unshift
+// ar.unshift("101","MD")
+// console.log(ar)
+// //shift
+// ar.shift(ar)
+// console.log(ar)
+//slice()
+// let arr=["ph",45,10.7,"MP"]
+// let ar1=arr.slice(1,3)
+// arr.splice(1,3)
+// arr.splice(2,0,"sir")
+// console.log(arr)
+
+// let ar=["cy",2,3,"N"]
+// let table=ar.slice(1,2)
+// console.log(table)
+// for(let i=1;i<=10;i++){
+//     console.log(table*i)
+// }
+
+// let ar1=[1,3,5,2,4,10,11,15]
+// let idx=parseInt(prompt("Enter idx"))
+// let el=parseInt(prompt("Enter number of el"))
+// ar1.splice(idx,el)
+// console.log(ar1)
