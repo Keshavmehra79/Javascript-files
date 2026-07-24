d5a888f72da9d929acc1f8eeb133734db51eacfd
keshavmehra79 <mehrakeshav332@gmail.com>
2026-07-24 15:53:27 +0530

    new

---
+++ Events.js
@@ -0,0 +1,19 @@
+//////////////////Event bubbling
+// let div=document.querySelector("div")
+
+// div.addEventListener("click",(event)=>{
+//     console.log("div.......")
+// })
+
+// let btn=document.querySelector("button")
+
+// btn.addEventListener("click",()=>{
+//     console.log("Button.....")
+// })
+
+
+/////////////Events capturing.........
+
+
+
+
