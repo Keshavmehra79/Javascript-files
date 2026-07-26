8f9c5628c2420689f091d80e53ac5aee30341973
keshavmehra79 <mehrakeshav332@gmail.com>
2026-07-26 16:34:27 +0530

    working.

--- Events.js
+++ Events.js
@@ -14,6 +14,48 @@
 
 /////////////Events capturing.........
 
+// let div=document.querySelector("div");
+// div.addEventListener("click",()=>{
+//     console.log("Div")
+// },true)
+// let btn=document.querySelector("button")
+// btn.addEventListener("click",()=>{
+//     console.log("Button")
+// },true)
 
+//////////stop propagation
+
+// let div=document.querySelector("div")
+
+// div.addEventListener("click",(event)=>{
+//     console.log("div.......")
+// })
+
+// let btn=document.querySelector("button")
+
+// btn.addEventListener("click",(event)=>{
+//         event.stopPropagation();
+//     console.log("Button.....")
+// })
+///////////immediatpropagtion
+// let btn=document.querySelector("button")
+
+// btn.addEventListener("click",(event)=>{
+//     event.stopImmediatePropagation();
+//     console.log("Button.....")
+// })
+
+// btn.addEventListener("click",(event)=>{
+//     console.log("Button2")
+// })
+
+
+///////////Event delegation
+let parent=document.getElementById("parent")
+parent.addEventListener("click",(event)=>{
+   if(event.target.classList.contains("btn")){
+    alert(`You clicked ${event.target.innerText}`)
+   }
+})
 
 
---
+++ closure.js
@@ -0,0 +1,32 @@
+///////////Closure
+// closure is a default behavior of js which inner function remember value of variable it's outer function even outer function has finished execution varaible has lexical scope so its value does/not lost
+
+// let sum=function(a){
+//     console.log("working..... "+a)
+//     var c=2
+//     return function(b){
+//         return a+b+c;
+//     }
+// }
+
+// var store=sum(200)
+// console.log(store(1))
+
+
+
+// let myfunc=function(a,b,c){
+//     return(
+//         {
+//             addTwosum:function(){
+//                 return a+b;
+//             },
+//             addThreesun:function(){
+//                 return a+b+c;
+//             }
+//         }
+//     )
+// }
+
+// let func=myfunc(1,2,3)
+// console.log(func.addTwosum())
+
